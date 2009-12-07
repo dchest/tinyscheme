033dfb9b4812b9edb617f1e922b1548f3fd757b9
Dmitry Chestnykh <dmitry@codingrobots.com>
2009-12-07 22:08:40 +0300

    Update test.scm.

--- TinySchemeObjC/test.scm
+++ TinySchemeObjC/test.scm
@@ -1,8 +1,8 @@
+(define (new class-name)
+        (objc-send (objc-send (objc-class class-name) "alloc") "init"))
+
 (begin
   (display "Hello")
-  
-  (define (new class-name)
-          (objc-send (objc-send (objc-class class-name) "alloc") "init")))
   
   (objc-send (new "Test") "displayObject:" 
                           "Hello from Test class instance!")
